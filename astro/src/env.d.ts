145a1f57bad0ee3c70c7ab6f7fe694c7e5bae663
Sergey Maslennikov <simasle@yandex.ru>
2024-11-21 11:50:35 +0300

    Add ClientID to ENV

--- astro/src/env.d.ts
+++ astro/src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+    readonly PUBLIC_UNSPLASH_CLIENT_ID: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
