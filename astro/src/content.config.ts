d8b654ef1aa1a66d3503932c99f1d775f7ded3b0
sergey.maslennikov <sergey.maslennikov@aliexpress.ru>
2024-12-27 14:32:57 +0300

    fix errors

--- astro/src/content.config.ts
+++ astro/src/content.config.ts
@@ -1,5 +1,5 @@
-import { defineCollection, z } from 'astro:content';
-import { glob, file } from 'astro/loaders';
+import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
     loader: glob({pattern: "**/*.md", base: "./src/pages/post"}),
