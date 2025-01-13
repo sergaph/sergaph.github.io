c29052f03cbed7393978960ea7eac2251fe1bd14
sergey.maslennikov <sergey.maslennikov@aliexpress.ru>
2025-01-13 10:37:19 +0300

    Add PhotoSphereViewer

--- astro/src/content.config.ts
+++ astro/src/content.config.ts
@@ -2,7 +2,11 @@ import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
-    loader: glob({pattern: "**/*.md", base: "./src/pages/post"}),
+    loader: glob({ pattern: "**/*.md", base: "./src/pages/post" }),
 });
 
-export const collections = { posts };
+const photos360 = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/pages/360" }),
+});
+
+export const collections = { posts, photos360 };
